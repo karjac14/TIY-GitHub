a17ae6058443816db9ed637b02f3881e53349046
Karlo Eleuterio <karloe@gmail.com>
2015-10-05 23:02:23 -0400

    repos default active, borders shows active tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,30 @@
 
-jQuery.ajax('../apis/github/users/octocat.json').then(function(data){
- console.log(arguments);
+
+
+//loads #repos nav as .active upon html is ready
+$(document).ready(function(){
+  $('#repos').addClass("active");
 });
+
+// set #contri as active
+$('#contri').on('click', function () {
+  this.addClass("actve");
+  $('#repos').removeClass("active");
+  $('#pubact').removeClass("active");
+});
+
+// set #pubact as active
+$('#pubact').on('click', function () {
+  this.addClass("actve");
+  $('#contri').removeClass("active");
+  $('#repos').removeClass("active");
+});
+
+
+
+
+
+// last week assignement
+// jQuery.ajax('../apis/github/users/octocat.json').then(function(data){
+//  console.log(arguments);
+// });
