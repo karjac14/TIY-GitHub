2d50e5376859afcf77f2e7a53a41bc76bfb7247d
Karlo Eleuterio <karloe@gmail.com>
2015-10-06 22:54:22 -0400

    added text from json files to the html

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,57 @@ $('#repos').on('click', function () {
 });
 
 
+var userApiUrl = "../../apis/github/users/karjac14.json";
+
+
+//jQuery.getJSON( url [, data ] [, success ] )
+
+// jQuery.getJSON(userApiUrl, function (json) {
+//   console.log ("Name: " + json.name);
+//
+//
+// })
+
+
+$.ajax({
+    url: userApiUrl,
+    dataType: 'json',
+    success: function( json )
+    {
+        $('#avatar').attr("src", json.avatar_url );
+        $('h1').text( json.name );
+        $('h2').text( json.login );
+        $('#company').text( json.company );
+        $('#location').text( json.location);
+        $('#blog').text( json.blog);
+        $('#email').text( json.email );
+        $('#created-at').text( json.created_at );
+        $('#followers').text( json.followers);
+        $('#starred').text( json.starred );
+        $('#following').text( json.following );
+
+        // using custom template delimiters
+        _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+        var name = _.template('{{name}}');
+        name(json);
+        // → 'hello mustache!'
+
+    }
+});
+
+$(document).ready(function (){
+  _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+  var names =_.template($("#temp-names").html());
+  var resultingNames = names(userApiUrl.name);
+
+  $('.prof-avatar').html(resultingNames);
+
+
+});
+
+
+
+
 
 
 // last week assignement
